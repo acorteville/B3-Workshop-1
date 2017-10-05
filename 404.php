37e7213290d4fca345f5967a86c5b8578d9419c4
Alexis Dubus <ielow.submarine@gmail.com>
2017-10-05 21:40:13 +0200

    rajout sur 404

--- 404.php
+++ 404.php
@@ -6,19 +6,25 @@
         <title>Erreur 404</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <script src="js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="Assets/CSS/bootstrap.css">
+        <script src="Assets/JS/jquery-3.2.1.js"></script>
+        <script src="Assets/JS/popper-utils.js"></script>
+        <script src="Assets/JS/bootstrap.min.js"></script>
+         <script src="Assets/JS/bootbox.min.js"></script>
+        <script src="Assets/JS/function.js"></script>
+        <link rel="stylesheet" href="Assets/CSS/style.css">
+        <link rel="stylesheet" href="Assets/CSS/font-awesome/css/font-awesome.min.css">
 
     </head>
     <body>
         <div class="header">
-            <h5 class="listetitle">Erreur 404 <a href="index.php"><i class="fa fa-home btnajout" aria-hidden="true"></i></a> </h5>
+            <h5 class="listetitle">Erreur 404  
+            <a href="logout.html" title="logout"><i class="fa fa-power-off btndisconnect" aria-hidden="true"></i></a>
+            <a href="index.php"><i class="fa fa-home btnajout" aria-hidden="true"></i></a></h5>
             
         </div>
         <div class="container">
-            <img src="Assets/IMG/404.jpg" alt="404" style="width: 100%;" class="loginlogo">
+            <img src="Assets/IMG/404-m.jpg" alt="404" style="width: 100%;" class="loginlogo">
         </div>
     </body>
 </html>
