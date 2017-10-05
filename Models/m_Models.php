686b5c7480ffebe6f75601f1ac8e92f50a577577
Bauduin Lucas <bauduin.lucas@gmail.com>
2017-10-05 14:18:03 +0200

    Petites mise à jours du modele (affichage des contacts dans l'ordre alphabetique)

--- Models/m_Models.php
+++ Models/m_Models.php
@@ -71,7 +71,7 @@ function getClients() {
 
 function getContacts($pid) {
     global $pdo;
-    $sql = "SELECT * FROM `contacts` WHERE `id_client` = ".filter($pid);
+    $sql = "SELECT * FROM `contacts` WHERE `id_client` = ".filter($pid)."ORDER BY name, lastname";
     $result = $pdo->prepare($sql);
     $result->execute();
     $contacts = array();
