abd578dec041c491aabb524030d849efb4133d94
Bauduin Lucas <bauduin.lucas@gmail.com>
2017-10-06 12:56:45 +0200

    Suppression controleur  et vue 404
    Suppression controleur  et vue 404

--- WebServices/index.php
+++ WebServices/index.php
@@ -1,5 +1,15 @@
 <?php
 
+/***
+ * Le WebService réponds aux requêtes passées en mote GET vers les liens suivants:
+ * 
+ * - Récupération des Besoins :
+ *  http://monsite.com/WebServices/?m=getrequirements&s=id&o=ASC
+ * 
+ * - Récupération des contacts d'un Client
+ *  http://monsite.com/WebServices/?m=getcontacts&id={id d'un client}
+ */
+
 include_once '../Classes/Pdom.php';
 include_once '../Classes/Client.php';
 include_once '../Classes/Contact.php';
@@ -11,7 +21,7 @@ Pdom::getPdo();
 $pdo = Pdom::getMonPdo();
 
 
-$method = isset($_GET['m']) ? $_GET['m'] : 'getrequirement';
+$method = isset($_GET['m']) ? $_GET['m'] : 'getrequirements';
 $sortingby = isset($_GET['s']) ? $_GET['s'] : 'id';
 $order = isset($_GET['o']) ? $_GET['o'] : 'ASC';
 $id = isset($_GET['id']) ? (int) $_GET['id'] : null;
@@ -20,7 +30,7 @@ $search = isset($_GET['s']) ? $_GET['s'] : null;
 
 
 switch ($method) {
-    case 'getrequirement':
+    case 'getrequirements':
         $requirements = getRequirements(1, NULL, $sortingby, $order, $search);
         echo json_encode($requirements);
         break;
